94f3aa88c1c297bc165295f9fab55d7f19be3a7a
KaterinaGreat <vasilevak@bk.ru>
2024-08-27 16:52:46 +0300

    добавила комментарий

--- module_2_5.py
+++ module_2_5.py
@@ -1,6 +1,11 @@
-def get_matrix (n, m, value):
+def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        number = [value]*m
-        matrix.append(number)
-print(get_matrix) #ghbtdn
+        matrix.append([])
+        for _ in range(m):
+            matrix[i].append(value)
+
+    return matrix
+rezul1 = print(get_matrix(2, 2, 10))
+rezul2 = print(get_matrix(3, 5, 42))
+rezul3 = print(get_matrix(4, 2, 13))
