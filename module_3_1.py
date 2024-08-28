64e2c26b6d099015d5f222c80ba89824de45a550
KaterinaGreat <vasilevak@bk.ru>
2024-08-28 11:01:36 +0300

    решение

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+def count_calls(calls):
+    print(calls)
+def string_info(string):
+    global calls
+    calls += 1
+    new_string = (len(string), string.upper(), string.lower())
+    return new_string
+def is_contains(string, list_to_search):
+    global calls
+    calls +=1
+    for i in list_to_search:
+        new_list = []
+        new_list.append(i.lower())
+        if string.lower() in new_list:
+            return True
+        else:
+            return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
