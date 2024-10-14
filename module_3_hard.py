78fb7268365e3b07621801892065235d47750a0e
KaterinaGreat <vasilevak@bk.ru>
2024-10-14 10:57:40 +0300

    решение обновлено

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,21 +1,21 @@
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
-def calculate_structure_sum(*args):
+def calculate_structure_sum(data):
     summa = 0
-    if isinstance(args, (dict)):
-        for key, value in args.items():
+    if isinstance(data, (dict)):
+        for key, value in data.items():
             summa += calculate_structure_sum(key)
             summa += calculate_structure_sum(value)
 
-    elif isinstance(args, (list, tuple, set)):
-        for item in args:
-            summa += calculate_structure_sum(item)
+    elif isinstance(data, (list, tuple, set)):
+        for i in data:
+            summa += calculate_structure_sum(i)
 
-    elif isinstance(args, (int, float)):
-        summa += args
+    elif isinstance(data, (int, float)):
+        return data
 
-    elif isinstance(args, str):
-        summa += len(args)
+    elif isinstance(data, str):
+        summa += len(data)
     return summa
 
 result = calculate_structure_sum(data_structure)
