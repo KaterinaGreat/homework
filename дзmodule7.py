4292c39d3c21259887cf94c1060c2a3fae170b2c
KaterinaGreat <vasilevak@bk.ru>
2024-12-14 08:30:35 +0300

    решение

--- дзmodule7.py
+++ дзmodule7.py
@@ -14,13 +14,12 @@ score_1 = 40
 print(f"Команды решили {score_1} и {score_2} задач.")
 team2_time = 2153.31451
 def challenge_result(result):
+    result = 'Ничья!'
     if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
         result = 'Победа команды Мастера кода!'
     elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
         result = 'Победа команды Волшебники Данных!'
-    else:
-        result = 'Ничья!'
-        return print(f"Результат битвы: {result}")
+    return print(f"Результат битвы: {result}")
 
 tasks_total = 82
 time_avg = (team1_time + team2_time)/ 2
